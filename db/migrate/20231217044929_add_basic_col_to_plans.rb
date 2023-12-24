e9388dbc5a83fe7cd2143a84c88ab85f97ec9e7f
trung <trung@trung.com>
2023-12-24 13:07:45 +0700

    change to new template

--- db/migrate/20231217044929_add_basic_col_to_plans.rb
+++
@@ -1,7 +0,0 @@
-class AddBasicColToPlans < ActiveRecord::Migration[7.1]
-  def change
-    add_column :plans, :name, :string
-    add_column :plans, :description, :text
-    add_column :plans, :user, :references, null: false, foreign_key: true
-  end
-end
